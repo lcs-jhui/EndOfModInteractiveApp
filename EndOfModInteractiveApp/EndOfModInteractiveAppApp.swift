818b53a9dc01030fafcc35735518299e48f4c681
Justin Hui <jhui@lcs.on.ca>
2023-02-07 11:29:40 -0500

    Initial Commit

---
+++ EndOfModInteractiveApp/EndOfModInteractiveAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EndOfModInteractiveAppApp.swift
+//  EndOfModInteractiveApp
+//
+//  Created by Justin Hui on 7/2/2023.
+//
+
+import SwiftUI
+
+@main
+struct EndOfModInteractiveAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
