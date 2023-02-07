5c98ecf61287e2322d8c399639e9d168d2371891
Justin Hui <jhui@lcs.on.ca>
2023-02-07 12:19:55 -0500

    Created the project and added a FindVelocity to replace the ContentView

---
+++ EndOfModInteractiveApp/Views/FindVelocity.swift
@@ -0,0 +1,20 @@
+//
+//  FindVelocity.swift
+//  EndOfModInteractiveApp
+//
+//  Created by Justin Hui on 7/2/2023.
+//
+
+import SwiftUI
+
+struct FindVelocity: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FindVelocity_Previews: PreviewProvider {
+    static var previews: some View {
+        FindVelocity()
+    }
+}
