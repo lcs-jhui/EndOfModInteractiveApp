dd122ccb5276f46a180b00b8c1d57afd37c53e54
Justin Hui <jhui@lcs.on.ca>
2023-02-08 18:01:10 -0500

    Added a new swift file called result

---
+++ EndOfModInteractiveApp/Model/Results.swift
@@ -0,0 +1,17 @@
+//
+//  Results.swift
+//  EndOfModInteractiveApp
+//
+//  Created by Justin Hui on 8/2/2023.
+//
+
+import Foundation
+
+struct VelcityResult: Identifiable {
+    
+    let id = UUID()
+    let velocity: String
+    let initialVelocity: String
+    let time: String
+    let acceleration: String
+}
