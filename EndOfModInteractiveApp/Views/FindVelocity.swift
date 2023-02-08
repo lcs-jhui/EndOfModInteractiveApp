dcd9309f438e588b19da16207b17cf309faabb36
Justin Hui <jhui@lcs.on.ca>
2023-02-08 16:00:54 -0500

    Added stored properties and computed properties. Computed properties is where Xcode calculates the velocity, and shows the calculated velocity result

--- EndOfModInteractiveApp/Views/FindVelocity.swift
+++ EndOfModInteractiveApp/Views/FindVelocity.swift
@@ -8,8 +8,60 @@
 import SwiftUI
 
 struct FindVelocity: View {
+    
+    //MARK: Stored Properties
+    @State var inputInitialVeolcity = ""
+    @State var inputTime = ""
+    @State var inputAcceleration = ""
+    
+    //MARK: Computed Properties
+    
+    //Calculate Velcotiy
+    var calculatedVelocity: Double? {
+        
+        //Try to convert initialVelocity to a Double
+        guard let initialVelocity = Double(inputInitialVeolcity) else {
+            return nil
+        }
+        
+        //Try to convert Time to a Double
+        guard let time = Double(inputTime) else {
+            return nil
+        }
+        
+        //Try to convert initialVelocity to a Double
+        guard let acceleration = Double(inputAcceleration) else {
+            return nil
+        }
+        
+        return initialVelocity + ( acceleration * time )
+    }
+    
+    //Show Result
+    var velocityResult: String {
+        
+        //Check if there is a valid velocity
+        guard let velocity = calculatedVelocity else {
+            //else provide an error message
+            return "Cannot be found. Please provide valid input"
+        }
+        //If there is a valid velocity
+        return velocity.formatted(.number.precision(.fractionLength(2)))
+    }
+    
+    
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            VStack{
+                
+                
+                
+                
+            }
+            .navigationTitle("Find The Velocity")
+        }
     }
 }
 
